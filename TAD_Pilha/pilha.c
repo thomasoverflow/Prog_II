78fd1888bfb6c474bbcdac3afac22f9ebf82ab97
Thomas_lab <thomascarmo@outlook.com>
2023-11-13 10:22:46 -0300

    Add new VPl

---
+++ TAD_Pilha/pilha.c
@@ -0,0 +1,5 @@
+//
+// Created by thomas on 13/11/23.
+//
+
+#include "pilha.h"
---
+++ TAD_Pilha/pilha.h
@@ -0,0 +1,8 @@
+//
+// Created by thomas on 13/11/23.
+//
+
+#ifndef PROG_II_PILHA_H
+#define PROG_II_PILHA_H
+
+#endif //PROG_II_PILHA_H
