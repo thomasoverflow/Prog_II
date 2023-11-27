684ed02553da137a9e2bff934310167c39789d7d
Thomas_lab <thomascarmo@outlook.com>
2023-11-27 13:06:26 -0300

    Add to repository new VPL

--- Arvore_bi/abb.c
+++ Arvore_bi/abb.c
@@ -50,66 +50,102 @@ int 		abb_insere		(abb_t *arv, elem_t chave){
 int 		abb_remove		(abb_t *arv, elem_t chave){
     if(!arv) return -1;
 
-    no_t **ptr = &(arv->raiz);
-    no_t **p = NULL;
+    no_t *ptr = arv->raiz;
+    no_t *p = NULL;
 
-    while(*ptr != NULL){
-        if(chave < (*ptr)->chave){
+    while(ptr != NULL){
+        if(chave < ptr->chave){
             p = ptr;
-            ptr = &((*ptr)->esq);
+            ptr = (ptr->esq);
         }
-        else if (chave > (*ptr)->chave){
+        else if (chave > ptr->chave){
             p = ptr;
-            ptr = &((*ptr)->dir);
+            ptr = (ptr->dir);
         }
         else{
             //Achamos o nó -> armazenado em ptr
             //1°Caso: Yo soy una FOLHA
-            if((*ptr)->esq == NULL && (*ptr)->dir == NULL) {
-                if((*p)->dir == NULL){
-                    free(*ptr);
-                    (*p)->esq = NULL;
+            if(ptr->esq == NULL && ptr->dir == NULL) {
+                if(p->dir == NULL){
+                    free(ptr);
+                    p->esq = NULL;
                 }else{
-                    free(*ptr);
-                    (*p)->dir = NULL;
+                    free(ptr);
+                    p->dir = NULL;
                 }
-            }else if((*ptr)->esq == NULL){//2a°Caso: Yo soy un nó apenas com un hijo a derecha (MUDAR)[
-                no_t *aux = (*ptr)->dir;
-                free(*ptr);
-                (*p)->dir = aux;
-            }else if((*ptr)->dir == NULL){//2b°Caso: Yo soy un nó apenas com un hijo a esquerda (MUDAR)
-                no_t *aux = (*ptr)->esq;
-                if((*ptr)->chave == (*p)->esq->chave){
-                    free(*ptr);
-                    (*p)->esq = aux;
+            }else if(ptr->esq == NULL){//2a°Caso: Yo soy un nó apenas com un hijo a derecha (MUDAR)[
+                no_t *aux = ptr->dir;
+                if(p->esq && p->dir){//
+                    if(ptr->chave == p->esq->chave){
+                        free(ptr);
+                        p->esq = aux;
+                    }else{
+                        free(ptr);
+                        p->dir = aux;
+                    }
                 }else{
-                    free(*ptr);
-                    (*p)->dir = aux;
+                    if(p->esq){
+                        free(ptr);
+                        p->esq = aux;
+                    }else{
+                        free(ptr);
+                        p->dir = aux;
+                    }
                 }
-
+            }else if(ptr->dir == NULL){//2b°Caso: Yo soy un nó apenas com un hijo a esquerda (MUDAR)
+                no_t *aux = ptr->esq;
+                if(p->esq && p->dir){//
+                    if(ptr->chave == p->esq->chave){
+                        free(ptr);
+                        p->esq = aux;
+                    }else{
+                        free(ptr);
+                        p->dir = aux;
+                    }
+                }else{
+                    if(p->esq){
+                        free(ptr);
+                        p->esq = aux;
+                    }else{
+                        free(ptr);
+                        p->dir = aux;
+                    }
+                }
             }else{//3°Caso: Yo tengo two hijoss
-                no_t **new = &((*ptr)->esq);
-
-                while((*new)->dir){
+                no_t *new = (ptr->esq);
+                int i = 0;
+                while((new)->dir){
                     p = new;
-                    new = &((*new)->dir);
+                    new = ((new)->dir);
+                    i++;
                 }//Chegamos no nó que irá substituir
-                if((*new)->dir){
-                    (*ptr)->chave = (*new)->chave;
-                    (*new)->chave = (*new)->dir->chave;
-                    free((*new)->dir);
-                    (*new)->dir = NULL;
+                if(i!=0){
+                    if((new)->esq){
+                        no_t *aux = (new)->esq;
+                        ptr->chave = (new)->chave;
+                        free(new);
+                        p->dir = aux;
+                    }else{//É folha
+                        ptr->chave = (new)->chave;
+                        free(new);
+                        p->dir = NULL;
+                    }
                 }else{
-                    (*ptr)->chave = (*new)->chave;
-                    free(*new);
-                    (*ptr)->esq = NULL;
-                    //(*p)->esq = NULL;
-                }//caso esteja na folha
-
+                    if((new)->esq){
+                        ptr->chave = (new)->chave;
+                        (new)->chave = (new)->esq->chave;
+                        free((new)->esq);
+                        (new)->esq = NULL;
+                    }else{//É folha
+                        ptr->chave = (new)->chave;
+                        free(new);
+                        ptr->esq = NULL;
+                    }
+                }
+                //caso esteja na folha
             }
             return 1;
         }
-
     }
     return 0;
 }//PARA TESTE
@@ -134,7 +170,7 @@ int 		abb_busca  		(abb_t *arv, elem_t chave){
 int main(){
    abb_t *l = abb_cria();
 
-    /*abb_insere(l,37);
+    abb_insere(l,37);
     abb_insere(l,59);
     abb_insere(l,13);
     abb_insere(l,42);
@@ -144,28 +180,39 @@ int main(){
     abb_insere(l,60);
     abb_insere(l,4);
     abb_insere(l,3);
-    abb_insere(l,5);*/
+    abb_insere(l,5);
     abb_insere(l,10);
     abb_insere(l,21);
     abb_insere(l,5);
+    abb_insere(l,9);
     abb_insere(l,15);
     abb_insere(l,23);
     abb_insere(l,30);
     abb_insere(l,3);
     abb_insere(l,7);
-    abb_insere(l,1);
+    abb_insere(l,8);
     abb_insere(l,6);
+    abb_insere(l,4);
+    abb_insere(l,22);
 
 
 
 
 
     int t = abb_busca(l,60);
-    int k = abb_remove(l,10);
-    //printf("%d\n",k);
-     //k = abb_remove(l,1);
-    //printf("%d\n",k);
-     //k = abb_remove(l,60);
+    int k;
+    //TESTA CASO 1
+    k = abb_remove(l,7);
+    //printf("%d",k);
+    //TESTA CASO 2
+    k = abb_remove(l,8);
+    //printf("%d",k);
+    //TESTA CASO 3
+    k = abb_remove(l,21);
+    //printf("%d",k);
+    //TESTA CASO 1
+    k = abb_remove(l,23);
+    k = abb_remove(l,42);
     printf("%d",k);
 
 
