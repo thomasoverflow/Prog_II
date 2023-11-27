925127dd91aee674b4f8b7c9fc40371ff0e86b3b
ThomasThinkpad <thomascarmo@outlook.com>
2023-11-26 23:51:56 -0300

    Código em testes, problema com remove (nota 7)

--- Arvore_bi/abb.c
+++ Arvore_bi/abb.c
@@ -51,43 +51,59 @@ int 		abb_remove		(abb_t *arv, elem_t chave){
     if(!arv) return -1;
 
     no_t **ptr = &(arv->raiz);
+    no_t **p = NULL;
 
     while(*ptr != NULL){
         if(chave < (*ptr)->chave){
+            p = ptr;
             ptr = &((*ptr)->esq);
         }
         else if (chave > (*ptr)->chave){
+            p = ptr;
             ptr = &((*ptr)->dir);
         }
         else{
             //Achamos o nó -> armazenado em ptr
             //1°Caso: Yo soy una FOLHA
             if((*ptr)->esq == NULL && (*ptr)->dir == NULL) {
-                free(*ptr);
-                *ptr = NULL;
+                if((*p)->dir == NULL){
+                    free(*ptr);
+                    (*p)->esq = NULL;
+                }else{
+                    free(*ptr);
+                    (*p)->dir = NULL;
+                }
             }else if((*ptr)->esq == NULL){//2a°Caso: Yo soy un nó apenas com un hijo a derecha
-                no_t *aux = *ptr;
-                *ptr = (*ptr)->dir;
-                free(aux);
+                no_t *aux = (*ptr)->dir;
+                free(*ptr);
+                (*p)->dir = aux;
             }else if((*ptr)->dir == NULL){//2b°Caso: Yo soy un nó apenas com un hijo a esquerda
-                no_t *aux = *ptr;
-                *ptr = (*ptr)->esq;
-                free(aux);
+                no_t *aux = (*ptr)->esq;
+                if((*ptr)->chave == (*p)->esq->chave){
+                    free(*ptr);
+                    (*p)->esq = aux;
+                }else{
+                    free(*ptr);
+                    (*p)->dir = aux;
+                }
+
             }else{//3°Caso: Yo tengo two hijoss
-                no_t **new = &((*ptr)->dir);
+                no_t **new = &((*ptr)->esq);
+
                 while((*new)->esq){
+                    p = new;
                     new = &((*new)->esq);
                 }//Chegamos no nó que irá substituir
                 if((*new)->dir){
-                    no_t *temp;
                     (*ptr)->chave = (*new)->chave;
-                    temp = (*new)->dir;
+                    (*new)->chave = (*new)->dir->chave;
+                    free((*new)->dir);
                     (*new)->dir = NULL;
-                    free(temp);
                 }else{
                     (*ptr)->chave = (*new)->chave;
-                    *new = NULL;
                     free(*new);
+                    (*ptr)->esq = NULL;
+                    //(*p)->esq = NULL;
                 }//caso esteja na folha
 
             }
@@ -115,13 +131,35 @@ int 		abb_busca  		(abb_t *arv, elem_t chave){
 
 
 int main(){
-    abb_t *l = abb_cria();
+   abb_t *l = abb_cria();
 
+    /*abb_insere(l,37);
+    abb_insere(l,59);
+    abb_insere(l,13);
+    abb_insere(l,42);
+    abb_insere(l,43);
+    abb_insere(l,27);
+    abb_insere(l,21);
+    abb_insere(l,60);
+    abb_insere(l,4);
     abb_insere(l,3);
-    abb_insere(l,1);
+    abb_insere(l,5);*/
+    abb_insere(l,10);
+    abb_insere(l,21);
     abb_insere(l,5);
-    abb_insere(l,6);
-    abb_insere(l,7);
+    abb_insere(l,15);
+    abb_insere(l,23);
+    abb_insere(l,30);
+
+
+
+    int t = abb_busca(l,60);
+    int k = abb_remove(l,23);
+    //printf("%d\n",k);
+     //k = abb_remove(l,1);
+    //printf("%d\n",k);
+     //k = abb_remove(l,60);
+    printf("%d",k);
 
 
     abb_destroi(&l);
